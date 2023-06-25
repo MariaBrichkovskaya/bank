96b1d38bf455103daeefd79ae909abe72ddc8540
MariaBrichkovskaya <brichkovskayam@gmail.com>
2023-06-25 13:04:51 +0300

    edit pom

--- eureka-server/src/main/java/com/bank/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/bank/eurekaserver/EurekaServerApplication.java
@@ -2,15 +2,13 @@ package com.bank.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-<<<<<<< HEAD
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
 @EnableEurekaServer
-=======
+
 
 @SpringBootApplication
->>>>>>> 24db7ac (first commit)
+
 public class EurekaServerApplication {
 
     public static void main(String[] args) {
